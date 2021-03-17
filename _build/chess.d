a19430b45e490d95da3091ce343dd5ab4aaf9729
Dulhan Jayalath <dhjayalath@protonmail.com>
2021-03-17 11:31:27 +0000

    Add rotary piece selector

--- _build/chess.d
+++ _build/chess.d
@@ -1,7 +1,9 @@
-_build/chess.o: chess.c unifiedLcd.h ili934x.h unifiedColor.h
+_build/chess.o: chess.c unifiedLcd.h ili934x.h unifiedColor.h rotary.h
 
 unifiedLcd.h:
 
 ili934x.h:
 
 unifiedColor.h:
+
+rotary.h:
--- chess.c
+++ chess.c
@@ -2,6 +2,7 @@
 #include <avr/io.h>
 #include <string.h>
 #include "unifiedLcd.h"
+#include "rotary.h"
 
 #define LT_SQ_COL compile(240, 217, 183)
 #define DK_SQ_COL compile(180, 135, 102)
@@ -31,15 +32,77 @@ void draw_board();
 void draw_credits();
 void init_pieces();
 void draw_pieces();
+void draw_square(uint8_t x, uint8_t y, uint16_t colour);
+void draw_piece(uint8_t x, uint8_t y);
 
 // State of the board
 uint8_t board[BOARD_SIZE][BOARD_SIZE];
+
+// Selected square
+volatile uint8_t sel_x = 0;
+volatile uint8_t sel_y = 0;
+volatile uint8_t sel_x_last = 0;
+volatile uint8_t sel_y_last = 0;
+
+volatile uint8_t redraw_select = 0;
+
+const char* display_pieces = " PNBRQKpnbrqk";
+
+
+ISR(TIMER1_COMPA_vect) {
+
+    if (rotary > 0) {
+
+        sel_x_last = sel_x;
+        sel_y_last = sel_y;
+
+        if (sel_x > 0) {
+            sel_x--;
+        } else {
+            if (sel_y > 0) {
+                sel_y--;
+                sel_x = 7;
+            }
+        }
+        redraw_select = 1;
+    }
+    if (rotary < 0) {
+
+        sel_x_last = sel_x;
+        sel_y_last = sel_y;
+
+        if (sel_x < 7) {
+            sel_x++;
+        } else {
+            if (sel_y < 7) {
+                sel_y++;
+                sel_x = 0;
+            }
+        }
+        redraw_select = 1;
+    }
+
+    rotary = 0;
+
+}
 
 int main() {
 
     // Turn off default prescaling of clock (AT90USB DS, p. 48)
     CLKPR = 1 << CLKPCE;
     CLKPR = 0;
+
+    // Setup rotary encoder
+    init_rotary();
+
+    // Enable rotary interrupt
+    EIMSK |= _BV(INT4) | _BV(INT5);
+
+    // Enable game timer interrupt (Timer 1 CTC Mode 4)
+	TCCR1A = 0;
+	TCCR1B = _BV(WGM12);
+	TCCR1B |= _BV(CS10);
+	TIMSK1 |= _BV(OCIE1A);
 
     // Setup screen I/O
     // Clock already prescaled so set clock option to 0
@@ -49,6 +112,35 @@ int main() {
     init_pieces();
     draw_pieces();
     //draw_credits();
+
+    sei();
+    for (;;) {
+
+        if (redraw_select) {
+
+            // Disable interrupts (display routine must NOT be disturbed)
+            cli();
+
+            // Redraw last square
+            uint16_t col = ((sel_x_last + sel_y_last) & 1) ? DK_SQ_COL : LT_SQ_COL;
+            draw_square(sel_x_last, sel_y_last, col);
+            draw_piece(sel_x_last, sel_y_last);
+
+            // Overwrite new square
+            draw_square(sel_x, sel_y, TURQUOISE);
+            draw_piece(sel_x, sel_y);
+
+            // Set flag to complete!
+            redraw_select = 0;
+
+            // Renable interrupts
+            sei();
+        }
+
+
+    }
+    cli();
+
 
 }
 
@@ -89,6 +181,18 @@ void draw_board() {
     }
 }
 
+void draw_square(uint8_t x, uint8_t y, uint16_t colour) {
+
+    rectangle r;
+    r.left = LEFT_OFFST + SQ_SIZE * x;
+    r.right = r.left + SQ_SIZE;
+    r.top = SQ_SIZE * y;
+    r.bottom = r.top + SQ_SIZE; 
+
+    fill_rectangle(r, colour);
+
+}
+
 void draw_credits() {
 
     display_curser_move(LEFT_OFFST, SQ_SIZE * BOARD_SIZE);
@@ -119,6 +223,7 @@ void init_pieces() {
     board[6][0] = B_KNIGHT;
     board[7][0] = B_ROOK;
 
+    // White pieces
     board[0][7] = W_ROOK;
     board[1][7] = W_KNIGHT;
     board[2][7] = W_BISHOP;
@@ -130,9 +235,14 @@ void init_pieces() {
 
 }
 
-void draw_pieces() {
+void draw_piece(uint8_t x, uint8_t y) {
+    if (board[x][y]) {
+        display_curser_move(LEFT_OFFST + x * SQ_SIZE + 7, y * SQ_SIZE + 7); 
+        display_char(display_pieces[board[x][y]]); 
+    }
+}
 
-    const char* display_pieces = " PNBRQKpnbrqk";
+void draw_pieces() {
 
     uint8_t i, j;
     for (i = 0; i < BOARD_SIZE; i++) {
@@ -144,6 +254,5 @@ void draw_pieces() {
                    
         }
     }
-
 
 }
